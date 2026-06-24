4708609c004758f8288d1b8a914b09731e8aa13a
Luana002 <correaluana623@gmail.com>
2026-06-24 01:11:07 -0300

    refactor: organização de componentes nas paginas principais e styles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,20 @@
 import Header from '../components/Header/Header'
+import { BtnEntrar } from "../components/Btn/Btn-entrar"
 import './LandingPage.css'
 import '../styles/variables.css'
 
 function LandingPage() {
     return (
         <>
-        <Header></Header>
+        <Header/>
         <div className='hero-content'>
-            <h1>Organize sua coleção de plantas</h1>
-            <h3>Cadastre suas espécies, monitore os cuidados diários e mantenha suas plantas crescendo saudáveis</h3>
+            <h1 className='heroTitle'>Organize sua coleção de plantas</h1>
+            <h3 className='heroSubtitle'>Cadastre suas espécies, monitore os cuidados diários e mantenha suas plantas crescendo saudáveis</h3>
+                <div className='heroButtonWrapper'>
+                    <BtnEntrar/>
+                </div>
         </div>
+
         </>
     );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import CardForm from '../components/form/CardForm'
+
+function Register() {
+  return (
+    <>
+      <CardForm />
+    </>
+  );
+}
+
+export default Register;
